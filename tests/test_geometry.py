b08888d8e7dbad9f567504fe1d73319b0dd01c22
Kushal Regmi <131018073+HydroSynapseKR@users.noreply.github.com>
2025-09-30 17:25:29 -0500

    Update test_geometry.py
    correcting the name of the class

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -1,5 +1,5 @@
-from hecras_tools.geometry_operations import GeometryOperations
+from hecras_tools.geometry_operations import GeometryHdf
 
 def test_init():
-    ops = GeometryOperations("dummy.hdf")
+    ops = GeometryHdf("dummy.hdf")
     assert ops.hdf_file == "dummy.hdf"
